67ad56c170bd2fd7fc1bc5f7db6423db63f45a92
readme42 <github@cschmale.de>
2013-09-24 23:17:07 +0200

    added: group, groupEnd, time, timeEnd, error, warn

--- js/debug.js
+++ js/debug.js
@@ -1,1 +1,29 @@
 console.log("hello world");
+
+console.time("Logging document");
+console.group("%cLogging document", "font-size: x-large" );
+
+    console.warn("console.log(document)");
+    console.log(document);
+
+    console.warn("console.dir(document)");
+    console.dir(document);
+
+console.groupEnd();
+console.timeEnd("Logging document");
+
+console.group("%cnested logging", "font-size: x-large");
+for (var x=0; x<2; x++) {
+    console.group("x%i",x);
+    for (var y=0; y<2; y++) {
+        console.group("x%iy%i",x,y);
+        console.error("x%iy%i %s",x,y,"has no content");
+        console.groupEnd();
+    }
+    console.groupEnd();
+}
+console.groupEnd();
+
+debugger;
+
+
